d0e9b27a8baf15bb57c3fdfb75d4bd051586d2a0
malcolm-smith <malcolm.henry.smith@gmail.com>
2018-11-27 11:14:43 -0500

    added android studio files

---
+++ OCSBApp2/app/src/main/java/com/example/malcolm/ocsbapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.malcolm.ocsbapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.textView);
+
+        text.setText(R.string.app_name);
+    }
+}
